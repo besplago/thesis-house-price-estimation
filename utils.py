7fbbe8d67bf18e5ff79dd221ee159c9024edaafd
emil565a <emil.hansen1998@gmail.com>
2024-02-21 09:06:53 +0100

    Added Ensemble notebook + other small things

--- utils.py
+++ utils.py
@@ -40,6 +40,17 @@ def plot_regression_results(model_name, y_test, y_pred):
     plt.show()
 
 
+def regression_stats(y_test, y_pred):
+    """Calculate regression statistics."""
+    from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+    mse = mean_squared_error(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    print(f'Mean Squared Error: {mse}')
+    print(f'Mean Absolute Error: {mae}')
+    print(f'R^2: {r2}')
+    return None
+
 def remove_outliers(df, column, z_score_threshold=2):
     """Remove outliers from a DataFrame based on a z-score threshold."""
     from scipy import stats
@@ -63,3 +74,5 @@ def print_DF_price_stats(df):
     print(f"Max price: {max_price}")
     print(f"Standard deviation of price:{std_price}")
     return None
+
+
